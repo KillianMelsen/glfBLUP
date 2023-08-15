90425fedcb79a6c3e38c75edefe2a43799120a47
Killian Melsen <killianmelsen@gmail.com>
2023-08-15 10:59:03 +0200

    added genetic-thomson-repdiv factor scores

--- R/factorScores.R
+++ R/factorScores.R
@@ -8,7 +8,7 @@
 #' @param loadings Loadings matrix.
 #' @param uniquenesses Uniquenesses matrix.
 #' @param m Latent dimension.
-#' @param type Type of factor scores to be calculated, can be \code{c("thomson", "genetic-thomson", "bartlett", "anderson")}.
+#' @param type Type of factor scores to be calculated, can be \code{c("thomson", "genetic-thomson", "genetic-thomson-repdiv", "bartlett", "anderson")}.
 #' @param Se \code{NULL} by default. Must be specified if \code{"genetic-thomson"} is used as the type.
 #'
 #' @return A dataframe with the factor scores.
@@ -18,11 +18,16 @@ factorScores <- function(data, loadings, uniquenesses, m, type = "thomson", Se =
 
   # Checks:
   stopifnot("Mismatch between loadings matrix and latent dimension!" = m == dim(loadings)[2])
-  if (type == "genetic-thomson") {
-    stopifnot("Se must be specified for 'genetic-thomson' factor scores" = !is.null(Se))
+  if (type %in% c("genetic-thomson", "genetic-thomson-repdiv")) {
+    stopifnot("Se must be specified for 'genetic-thomson' or 'genetic-thomson-repdiv' factor scores" = !is.null(Se))
   }
 
   datamat <- as.matrix(data[, -1])
+
+  if (type == "genetic-thomson-repdiv") {
+    n.rep.vector <- as.integer(table(data[, 1]))
+    reps <- (sum(n.rep.vector) - sum(n.rep.vector^2) / sum(n.rep.vector)) / (length(n.rep.vector) - 1)
+  }
 
   # Filling the dataframe:
   if (type == "thomson"){
@@ -37,6 +42,9 @@ factorScores <- function(data, loadings, uniquenesses, m, type = "thomson", Se =
     scores <- as.data.frame(datamat %*% psi.i %*% loadings %*% expm::sqrtm(solve((G %*% (diag(m) + G)))))
   } else if (type == "genetic-thomson") {
     A.i <- solve(uniquenesses + Se)
+    scores <- as.data.frame(datamat %*% A.i %*% loadings %*% solve(diag(m) + t(loadings) %*% A.i %*% loadings))
+  } else if (type == "genetic-thomson-repdiv") {
+    A.i <- solve(uniquenesses + (Se / reps))
     scores <- as.data.frame(datamat %*% A.i %*% loadings %*% solve(diag(m) + t(loadings) %*% A.i %*% loadings))
   }
 
